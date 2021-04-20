f3af6cb4e769fde098e405ca50a3cadd9e398537
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-04-20 21:40:19 +0300

    edit post notification added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
 
     if @post.update(post_params)
-      redirect_to @post
+      redirect_to @post, notice: "Your post has been successfully edited!"
     else
       render :edit
     end
