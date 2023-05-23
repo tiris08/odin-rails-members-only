557fb54f6cb967433fa9e77bb24ea082a2d764e9
Taras <ivanyuk.taras@gmail.com>
2023-05-22 19:50:43 -0500

    Add before_action to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-      if @comment.save
-        redirect_to post_path(@post), notice: "Your comment was successfully created!"
-      else
-        redirect_to post_path(@post), alert: "Comment was not created"
-      end
+
+    if @comment.save
+      redirect_to post_path(@post), notice: "Your comment was successfully created!"
+    else
+      redirect_to post_path(@post), alert: "Comment was not created"
+    end
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post), notice: "Your comment was successfully deleted!"
   end
-  
+
   private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:content, :user_id)
